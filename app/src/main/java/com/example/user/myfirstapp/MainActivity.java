1989c23693a17e68558a776ba629a1c956994408
Basmkb <basgenz11@gmail.com>
2017-02-16 14:48:34 +0700

    Batton MorelnfoComplete

--- app/src/main/java/com/example/user/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/user/myfirstapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.myfirstapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }//main Method
+    public void onCickMoreInfo(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.dlt.go.th/th/dlt-knowledge/view.php?_did=111"));
+        startActivity(intent);
     }
-}
+
+} //main Class
